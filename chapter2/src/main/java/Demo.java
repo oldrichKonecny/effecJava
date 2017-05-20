3267786b2a47e819e14d17b25aec335618171b66
oldrich.konecny <oldrich.konecny@ibacz.eu>
2017-05-20 09:39:52 +0200

    Added chapter2 modue + .gitignore file

---
+++ chapter2/src/main/java/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by oldrichkonecny on 20/05/2017.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ chapter2/src/main/java/StaticFactory.java
@@ -0,0 +1,8 @@
+/**
+ * Created by oldrichkonecny on 20/05/2017.
+ */
+public class StaticFactory {
+
+    
+
+}
